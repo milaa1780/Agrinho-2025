258dd954bf5ec1ac8484e683975b0a8f9b18d63f
milaa1780 <emilia.cardoso@escola.pr.gov.br>
2025-06-17 12:08:51 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var img01 
+
+var xJogador1=80
+var xJogador2=180
+
+function setup() {
+  createCanvas(800, 600);
+  
+  img01=loadImage("4089327023_c3cbf870c4_b.jpg");
+}
+
+
+
+function draw() {
+  background(img01);
+  
+  textSize(75)
+  text("🚶‍♀️", xJogador1,300)
+  text("🏃‍", xJogador2,)
+  
+  
+}
+ 
